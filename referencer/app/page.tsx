696f2b847886f40bffa849bad9347264a65104c4
rsvedant <iamvedantsingh1407@gmail.com>
2025-07-19 20:26:46 -0700

    initial setup for auth + app schema

---
+++ referencer/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      ts is da referencer app.
+    </div>
+  )
+}
---
+++ referencer/lib/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@/generated/prisma'
+import { PrismaNeon } from '@prisma/adapter-neon'
+const connectionString = `${process.env.DATABASE_URL}`
+
+export const adapter = new PrismaNeon({ connectionString })
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient({ adapter })
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
