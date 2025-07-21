dd788063048c3d1f1fee400711f34899a80a83f0
rsvedant <iamvedantsingh1407@gmail.com>
2025-07-21 12:16:18 -0700

    setup extension and GH actions workflow

--- referencer/lib/prisma.ts
+++ referencer/lib/prisma.ts
@@ -4,9 +4,8 @@ const connectionString = `${process.env.DATABASE_URL}`
 
 export const adapter = new PrismaNeon({ connectionString })
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ adapter })
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
