b8c2fa9754999a82dff52f736424069fcc356760
rsvedant <iamvedantsingh1407@gmail.com>
2025-07-30 15:27:36 -0700

    revert to original logic + new allowedOrigins

--- referencer/lib/cors.ts
+++ referencer/lib/cors.ts
@@ -1,8 +1,12 @@
 export const allowedOrigins = [
     "chrome-extension://gnnmpolacegkhdellgjmpjbmnhabloop",
     "http://localhost:3000", 
+    "https://www.ytref.vercel.app",
+    "https://www.makemeramovie.com",
     "https://ytref.vercel.app",
     "https://makemeramovie.com",
     "http://ytref.vercel.app",
     "http://makemeramovie.com",
+    "http://www.ytref.vercel.app",
+    "http://www.makemeramovie.com",
 ]
